fc51e87f6f89496b486dbee43d67abb16ab16abf
LegendPhoenix <louis@hopp.lu>
2024-04-29 11:29:37 +0200

    removed unused function used for debugging

--- push_swap.c
+++ push_swap.c
@@ -12,37 +12,6 @@
 
 #include "push_swap.h"
 
-// remove later, just to test if the stack was created correctly
-void print_stacks(t_stack *stack_a, t_stack *stack_b)
-{
-	// print the stack to verify that it was created correctly
-	printf("stack_a\n");
-	if (stack_a == NULL) {
-		printf("stack is empty\n");
-	} else {
-		t_stack *temp = stack_a;
-		while (temp->next != stack_a) {
-			printf("%ld\n", temp->value);
-			temp = temp->next;
-		}
-		printf("%ld\n", temp->value);
-		printf("end\n\n");
-	}
-
-	printf("stack_b\n");
-	if (stack_b == NULL) {
-		printf("stack is empty\n");
-	} else {
-		t_stack *temp = stack_b;
-		while (temp->next != stack_b) {
-			printf("%ld\n", temp->value);
-			temp = temp->next;
-		}
-		printf("%ld\n", temp->value);
-		printf("end\n\n");
-	}
-}
-
 // initialize stacks, which are actually doubly linked lists
 int main(int argc, char *argv[])
 {
